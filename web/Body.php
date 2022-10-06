b4e109d8ae59aacc3cb0a8a120e963d686c6611a
reinaagesti02 <reinaagesti02@gmail.com>
2022-10-06 23:07:13 +0700

    WEB

---
+++ web/Body.php
@@ -0,0 +1,315 @@
+
+    <!-- //ab -->
+    <!--/ab -->
+    <section class="about py-5">
+        <div class="container pb-lg-3">
+            <h3 class="tittle text-center">TERBARU</h3>
+            <div class="row">
+                <div class="col-md-4 product-men">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s1.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Bella Toes </a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$675.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s2.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Chikku Loafers </a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$475.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s3.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">(SRV) Sneakers </a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$575.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men my-lg-4">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s4.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Shuberry Heels</a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$575.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men my-lg-4">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s5.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Red Bellies </a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$575.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men my-lg-4">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s6.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Catwalk Flats </a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$575.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s7.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Running Shoes </a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$675.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s8.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Sukun Casuals </a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price ">
+                                    <span class="money ">$775.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 product-men">
+                    <div class="product-shoe-info shoe text-center">
+                        <div class="men-thumb-item">
+                            <img src="images/s9.jpg" class="img-fluid" alt="">
+                            <span class="product-new-top">New</span>
+                        </div>
+                        <div class="item-info-product">
+                            <h4>
+                                <a href="shop-single.html">Bank Sneakers</a>
+                            </h4>
+
+                            <div class="product_price">
+                                <div class="grid-price">
+                                    <span class="money">$875.00</span>
+                                </div>
+                            </div>
+                            <ul class="stars">
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>
+                                <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- //ab -->
+    <!--/testimonials-->
+    <section class="testimonials py-5">
+        <div class="container">
+            <div class="test-info text-center">
+                <h3 class="my-md-2 my-3">Jenifer Burns</h3>
+
+                <ul class="list-unstyled w3layouts-icons clients">
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-star"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-star"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-star"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-star-half-o"></span>
+                        </a>
+                    </li>
+                </ul>
+                <p><span class="fa fa-quote-left"></span> Produknya sangat bagus dan tidak mengecewakan <span class="fa fa-quote-right"></span></p>
+
+            </div>
+        </div>
+    </section>
+    <!--//testimonials-->
+    <!--/ab -->
+    <section class="about py-5">
+        <div class="container pb-lg-3">
+            <h3 class="tittle text-center">PRODUK POPULER</h3>
+            <div class="row">
+                <div class="col-md-6 latest-left">
+                    <div class="product-shoe-info shoe text-center">
+                        <img src="images/img1.jpg" class="img-fluid" alt="">
+                        <div class="shop-now"><a href="shop.html" class="btn">Shop Now</a></div>
+                    </div>
+                </div>
+                <div class="col-md-6 latest-right">
+                    <div class="row latest-grids">
+                        <div class="latest-grid1 product-men col-12">
+                            <div class="product-shoe-info shoe text-center">
+                                <div class="men-thumb-item">
+                                    <img src="images/img2.jpg" class="img-fluid" alt="">
+                                    <div class="shop-now"><a href="shop.html" class="btn">Shop Now</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="latest-grid2 product-men col-12 mt-lg-4">
+                            <div class="product-shoe-info shoe text-center">
+                                <div class="men-thumb-item">
+                                    <img src="images/img3.jpg" class="img-fluid" alt="">
+                                    <div class="shop-now"><a href="shop.html" class="btn">Shop Now</a></div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ web/Header.php
@@ -0,0 +1,132 @@
+
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Bootie Ecommerce Bootstrap Responsive Web Template | Home :: W3layouts</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="Bootie Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script>
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- //Meta tag Keywords -->
+
+    <!-- Custom-Files -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Bootstrap-Core-CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <!-- Style-CSS -->
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome-icons -->
+    <!-- /Fonts -->
+    <link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
+    <!-- //Fonts -->
+
+</head>
+
+<body>
+
+    <!-- mian-content -->
+    <div class="main-banner" id="home">
+        <!-- header -->
+        <header class="header">
+            <div class="container-fluid px-lg-5">
+                <!-- nav -->
+                <nav class="py-4">
+                    <div id="logo">
+                        <h1> <a href="index.html"><span class="fa fa-bold" aria-hidden="true"></span>pompies</a></h1>
+                    </div>
+
+                    <label for="drop" class="toggle">Menu</label>
+                    <input type="checkbox" id="drop" />
+                    <ul class="menu mt-2">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li>
+                            <!-- First Tier Drop Down -->
+                            <label for="drop-2" class="toggle">Drop Down <span class="fa fa-angle-down" aria-hidden="true"></span> </label>
+                            <a href="#">Drop Down <span class="fa fa-angle-down" aria-hidden="true"></span></a>
+                            <input type="checkbox" id="drop-2" />
+                            <ul>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="shop.html">Shop Now</a></li>
+                                <li><a href="shop-single.html">Single Page</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+                <!-- //nav -->
+            </div>
+        </header>
+        <!-- //header -->
+        <!--/banner-->
+        <div class="banner-info">
+            <p>Trending Terkini</p>
+            <h3 class="mb-4">Sepatu Kasual Pria & Wanita</h3>
+            <div class="ban-buttons">
+                <a href="shop-single.html" class="btn">Shop Now</a>
+                <a href="single.html" class="btn active">Read More</a>
+            </div>
+        </div>
+        <!--// banner-inner -->
+
+    </div>
+    <!--//main-content-->
+    <!--/ab -->
+    <section class="about py-md-5 py-5">
+        <div class="container-fluid">
+            <div class="feature-grids row px-3">
+                <div class="col-lg-3 gd-bottom">
+                    <div class="bottom-gd row">
+                        <div class="icon-gd col-md-3 text-center"><span class="fa fa-truck" aria-hidden="true"></span></div>
+                        <div class="icon-gd-info col-md-9">
+                            <h3 class="mb-2">BEBAS BIAYA KIRIM</h3>
+                            <p>On all order over $2000</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 gd-bottom">
+                    <div class="bottom-gd row bottom-gd2-active">
+                        <div class="icon-gd col-md-3 text-center"><span class="fa fa-bullhorn" aria-hidden="true"></span></div>
+                        <div class="icon-gd-info col-md-9">
+                            <h3 class="mb-2">PENGEMBALIAN GRATIS</h3>
+                            <p>On 1st exchange in 30 days</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 gd-bottom">
+                    <div class="bottom-gd row">
+                        <div class="icon-gd col-md-3 text-center"> <span class="fa fa-gift" aria-hidden="true"></span></div>
+
+                        <div class="icon-gd-info col-md-9">
+                            <h3 class="mb-2">MEMBER DISCOUNT</h3>
+                            <p>Register & save up to $29%</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-lg-3 gd-bottom">
+                    <div class="bottom-gd row">
+                        <div class="icon-gd col-md-3 text-center"> <span class="fa fa-usd" aria-hidden="true"></span></div>
+                        <div class="icon-gd-info col-md-9">
+                            <h3 class="mb-2">PREMIUM SUPPORT</h3>
+                            <p>Support 24 hours per day</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
